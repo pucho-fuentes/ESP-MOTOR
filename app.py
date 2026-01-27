2d524ae6bfbb06330fa42350350e18bce4307fba
pucho-fuentes <164549176+pucho-fuentes@users.noreply.github.com>
2026-01-27 13:52:44 -0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
-
+import os
 app = Flask(__name__)
 
 # Guardar último dato recibido
@@ -37,4 +37,5 @@ def send_data():
     return jsonify(latest_data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
